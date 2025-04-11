0076c330cfd662b30f1a047af079314f007cac10
jay4codes <jay4codes@gmail.com>
2025-04-11 18:52:37 +0530

    minr fix

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ from typing import Optional, Dict
 import asyncio
 from pathlib import Path
 from pydantic import BaseModel
-from .utils import (
+from utils import (
     read_file,
     get_unnamed_columns,
     get_mismatched_columns,
