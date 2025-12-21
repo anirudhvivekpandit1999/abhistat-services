974b30d055f46fa11fe583d1029c9c043103d4a2
Jay Jain <jay4codes@gmail.com>
2025-12-21 22:34:23 +0530

    shell fix

--- start.sh
+++ start.sh
@@ -6,4 +6,4 @@ source venv/bin/activate
 
 export PYTHONPATH=/root/Abhitech_Statistical_Tool_Backend:$PYTHONPATH
 
-exec uvicorn main:app --host 0.0.0.0 --port 8000 --workers 4
+exec uvicorn main:app --host 127.0.0.1 --port 8000 --workers 4
